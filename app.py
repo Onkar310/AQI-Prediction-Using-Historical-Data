580bc54ceccbbccc3a4430e5347a9c3f6503cf6f
Onkar310 <105639953+Onkar310@users.noreply.github.com>
2024-04-18 01:47:04 +0530

    Add files via upload
    Changed the model and now predicting all pollutant values along with AQI

--- app.py
+++ app.py
@@ -1,44 +1,50 @@
-
-
-
 from flask import Flask, render_template, request, jsonify
-import pickle
 import pandas as pd
+import pickle
 
 app = Flask(__name__)
 
-# Load the trained model
+# Load the models and prediction function from the pickle file
 with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
+    models = pickle.load(f)
 
-# Define route for the index page
+# Function to predict hourly values for a given date
+def predict_hourly_values(date_str):
+    date = pd.to_datetime(date_str, format='%Y-%m-%d')
+    hourly_predictions = []
+    for pollutant, model in models.items():
+        # Create a DataFrame with the hourly timestamps for prediction
+        hours_to_predict = pd.date_range(date + pd.Timedelta(hours=10), date + pd.Timedelta(hours=18), freq='H')
+        future = pd.DataFrame({'ds': hours_to_predict})
+
+        # Make predictions
+        forecast = model.predict(future)
+
+        # Get the predicted values for the specified hours and convert to list format
+        values = forecast['yhat'].tolist()
+
+        # Append the pollutant name as the first element of the list
+        values.insert(0, pollutant)
+
+        # Append the list of predicted values to the hourly_predictions list
+        hourly_predictions.append(values)
+
+    return hourly_predictions
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Define route for prediction
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the selected date from the form
-    selected_date = request.form['date']
+    # Get the date input from the form
+    user_date = request.form['date']
 
-    # Convert the selected date to datetime format
-    date_to_predict = pd.to_datetime(selected_date, format='%Y-%m-%d')
-
-    # Create a DataFrame with the hourly timestamps for prediction
-    hours_to_predict = pd.date_range(date_to_predict + pd.Timedelta(hours=10), date_to_predict + pd.Timedelta(hours=18), freq='H')
-    future = pd.DataFrame({'ds': hours_to_predict})
-
-    # Make prediction
-    forecast = model.predict(future)
-
-    # Get the predicted AQI values for the specified hours
-    hourly_aqi = forecast[['ds', 'yhat']].rename(columns={'ds': 'date', 'yhat': 'aqi'})
-
-    # Convert hourly AQI values to array format
-    hourly_aqi_array = hourly_aqi.to_dict(orient='records')
-
-    return jsonify(hourly_aqi_array)
+    # Predict hourly values for the user input date
+    predictions = predict_hourly_values(user_date)
+    print(predictions)
+    # Return the predictions as JSON response
+    return jsonify(predictions)
 
 if __name__ == '__main__':
     app.run(debug=True)
